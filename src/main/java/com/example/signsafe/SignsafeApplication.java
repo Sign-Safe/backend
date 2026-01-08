fce74861aba337e74c21aa15393e04d9d869cb16
Janghawnhui <68009882+Janghawnhui@users.noreply.github.com>
2026-01-08 23:53:47 +0900

    First Commit

---
+++ src/main/java/com/example/signsafe/SignsafeApplication.java
@@ -0,0 +1,13 @@
+package com.example.signsafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignsafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignsafeApplication.class, args);
+	}
+
+}
